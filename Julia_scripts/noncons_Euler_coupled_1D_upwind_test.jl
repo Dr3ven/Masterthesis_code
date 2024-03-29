c53046c69ea6994b33899e5d8e6ab9079f770cc0
Nils Medinger <NilsMedinger@gmx.de>
2024-03-29 16:31:15 +0100

    some progress

--- Julia_scripts/noncons_Euler_coupled_1D_upwind_test.jl
+++ Julia_scripts/noncons_Euler_coupled_1D_upwind_test.jl
@@ -1,4 +1,5 @@
 using CairoMakie
+using Infiltrator
 using SodShockTube
 
 function av_x(B)
@@ -8,7 +9,6 @@ end
 function upwind(u, G, dx)
     G_p = (G[2:end-1] .- G[1:end-2])./dx
     G_m = (G[2:end-1] .+ G[3:end])./dx
-    #@infiltrate
     Gflux = (G_p.*(u[2:end-1] .> 0) .+ G_m.*(u[2:end-1] .< 0))
 
     return Gflux
@@ -30,7 +30,8 @@ function coupled_wave1D_up()
     Lx = 1000.0                           # domain
     γ = 1.4                                # adiabatic index/ratio of specific heats
     K = 1.0e10                             # shear modulus
-    ρ0 = 3000.0                          # initial density at all points
+    β = 1.0 / K
+    ρ0 = 2800.0                          # initial density at all points
     P0_a = 1.0e5                          # initial pressure at all points
     P0_s = 1.0e6
     P0_c = P0_s + 1.0e7
@@ -40,11 +41,11 @@ function coupled_wave1D_up()
     σ = Lx * 0.04                            # standard deviation of the initial pressure distribution
     
     # Plotting parameters
-    divisor = 100 
+    divisor = 10
     plotlegend = false
 
     # Numerics
-    nx = 100                             # number of nodes in x
+    nx = 200                             # number of nodes in x
     dx = Lx / nx                        # step size in x
     nt = 700000                             # number of time steps
 
@@ -53,7 +54,7 @@ function coupled_wave1D_up()
     xv = 0:dx:Lx        # grid vertices in x-direction
 
     # Allocations
-    β = zeros(nx)
+    #β = zeros(nx)
     ρ = ones(nx)
     ρ_old = zeros(nx)
     ρ_t_av = zeros(nx)
@@ -80,33 +81,46 @@ function coupled_wave1D_up()
     PdVxdx = zeros(nx - 2)
     ρVxdEdx = zeros(nx - 2)
 
+    idx_solid = Int(floor((33/100)*nx))
+    idx_chamber = idx_solid+1
+    idx_chamber2 = Int(floor((65/100)*nx))
+    idx_air = idx_chamber2+1
+
     masksolid = ones(nx)
+    masksolidVx = ones(nx+1)
     maskair = masksolid
-    maskair[1:Int((33/100)*nx)] .= 0.0
-    masksolid[Int((34/100)*nx):end] .= 0.0
+    maskairVx = masksolidVx
+    maskair[1:idx_solid] .= 0.0
+    maskairVx[1:idx_solid] .= 0.0
+    masksolid[idx_chamber:end] .= 0.0
+    masksolidVx[idx_chamber:end] .= 0.0
     
     # Initial conditions
     #P .= P0 .+ A .* exp.(.- 1.0 .* (xc ./ σ).^2.0)       # initial pressure distribution
-    depth_solid = 67:-1:1
-    depth_air = 1:1:35
+    #depth_solid = 67:-1:1
+    depth_air = 1:1:length(idx_air:nx)
+
     # IC for solid
     # Constant solid density ρ = 3000.0
     # Lithosstatic pressure P = P0 + ρgh
-    ρ[1:Int((33/100)*nx)] .= 2800.0
-    P[1:Int((33/100)*nx)] .= P0_s #.+ (ρ[1:Int((33/100)*nx)] .* 9.81 .* reverse(Vector(34:1:66)))
+    ρ[1:idx_solid] .= ρ0
+    P[1:idx_solid] .= P0_s #.+ (ρ[1:idx_solid] .* 9.81 .* reverse(Vector(34:1:66)))
     # IC for chamber
     # Constant chamber ideal gas density ρ = 2800.0 
     # Ideal gas pressure P = P0 *  ρ / ρ0
-    ρ[Int((34/100)*nx):65] .= 2800.0 
-    P[Int((34/100)*nx):65] .= P0_c #.* ρ[Int((34/100)*nx):65] ./ 2800 
+    ρ[idx_chamber:idx_chamber2] .= ρ0 .+ 3.0
+    P[idx_chamber:idx_chamber2] .= P0_c #.* ρ[idx_chamber:idx_chamber2] ./ 2800 
+    E[idx_chamber:idx_chamber2] .= P[idx_chamber:idx_chamber2] ./ ((γ - 1.0)) .+ 0.5 .* av_x(Vx)[idx_chamber:idx_chamber2].^2
+    e[idx_chamber:idx_chamber2] .= P[idx_chamber:idx_chamber2] ./ (γ - 1.0) ./ ρ[idx_chamber:idx_chamber2]
+    #P[idx_chamber:idx_chamber2] .= (γ .- 1.0) .* (E[idx_chamber:idx_chamber2] .- 0.5 .* ρ[idx_chamber:idx_chamber2] .* av_x(Vx)[idx_chamber:idx_chamber2].^2)#    P0_c #.* ρ[idx_chamber:idx_chamber2] ./ 2800 
     # IC for air
     # Constant ideal gas density ρ = 1.225
     # Ideal gas pressure P = P0 * exp(-ghM/RT)
-    ρ[Int((66/100)*nx):end] .= 1.225
-    P[Int((66/100)*nx):end] .= P0_a .* exp.(-9.81 .* depth_air .* 0.028 ./ 288.0 ./ 8.314)
+    ρ[idx_air:end] .= 1.225
+    P[idx_air:end] .= P0_a .* exp.(-9.81 .* depth_air .* 0.028 ./ 288.0 ./ 8.314)
     c = sqrt(K / ρ0)                # speed of sound
     E .= P ./ ((γ - 1.0)) + 0.5 .* av_x(Vx).^2
-    e = P ./ (γ - 1.0) ./ ρ
+    e .= P ./ (γ - 1.0) ./ ρ
 
     dt = 1.0e-4#8 #dx / (c * 4.0)                      # time step size
     t = 0.0                                         # initial time
@@ -140,15 +154,15 @@ function coupled_wave1D_up()
     display(fig)
 
     for i = 1:nt
-        β .= 1.0 ./ P
+        #β .= 1.0 ./ P
         ρ_old .= ρ
         Mx_old .= Mx
         P_old .= P
-        c_var_s = sqrt(K ./ maximum(ρ[1:33]))
-        c_var_a = sqrt(K ./ maximum(ρ[34:end]))
+        c_var_s = sqrt(K ./ maximum(ρ[1:idx_solid]))
+        c_var_c = sqrt(K ./ maximum(ρ[idx_chamber:end]))
 
-        Vxdρdx .= .-av_x(Vx[2:end-1]) .* upwind_center(Vx, ρ, dx) .* maskair[2:end-1]
-        ρdVxdt .= .-ρ[2:end-1] .* (diff(Vx[2:end-1], dims=1) ./ dx) .* maskair[2:end-1]
+        Vxdρdx .= .-av_x(Vx[2:end-1]) .* upwind_center(Vx, ρ, dx) #.* maskair[2:end-1]
+        ρdVxdt .= .-ρ[2:end-1] .* (diff(Vx[2:end-1], dims=1) ./ dx) #.* maskair[2:end-1]
         dρ = Vxdρdx .* dt .+ ρdVxdt .* dt
         ρ[2:end-1] .= ρ[2:end-1] .+ dρ
         ρ_t_av .= (ρ .+ ρ_old) .* 0.5
@@ -162,7 +176,7 @@ function coupled_wave1D_up()
         Vxdρdt .= .-(1.0 ./ av_x(ρ)) .* Vx[2:end-1] .* (dρ_t ./ dt)
         ρdPdx .= .-(1.0 ./ av_x(ρ)) .* diff(P, dims=1) ./ dx
         ρVxdVxdx .= .-(1.0 ./ av_x(ρ)) .* (av_x(ρ) .* Vx[2:end-1]) .* diff(av_x(Vx), dims=1) ./ dx
-        VxdρVxdx .= .-(1.0 ./ av_x(ρ)) .* Vx[2:end-1] .* upwind(Vx, ρ_v .* Vx, dx)
+        VxdρVxdx .= .-(1.0 ./ av_x(ρ)) .* Vx[2:end-1] .* upwind(Vx, ρ_v .* Vx, dx) .* maskairVx[2:end-1]
         Vx[2:end-1] .= Vx[2:end-1] .+ ρdPdx .* dt .+ ρVxdVxdx .* dt .+ VxdρVxdx .* dt .+ Vxdρdt .* dt
         
         Vx[1] = Vx[2]
@@ -193,32 +207,38 @@ function coupled_wave1D_up()
         # E[2:end-1] .= E[2:end-1] .+ EdρVxdx .* dt .+ VxdPdx .* dt .+ PdVxdx .* dt .+ ρVxdEdx .* dt
 
         if any(ρ .< 0.0)
-            #println("Negative density at time $t")
-            #@show findall(x-> x .< 0.0, ρ)
+            println("Negative density at iteration $i, time $t")
+            findρ = findall(x-> x .< 0.0, ρ)
+            #println("Index $findρ\nDensity: $(ρ[findρ])")
+            break
+        elseif any(P .< -1.0e6)
+            findP = findall(x-> x .< -1.0e6, P)
+            #println("Negative pressure at iteration $i, time $t")
+            #println("Index $findP\nPressure: $(P[findP])")
             #break
-        elseif any(e .< 0.0)
+        elseif any(abs.(Vx) .> 10000.0)
+            println("Unrealistic velocity at iteration $i, time $t")
+            findVx = findall(x-> x .> 10000.0, abs.(Vx))
+            #println("Index $findVx\nVelocity: $(Vx[findVx])")
+            #break
+        #elseif any(e .< 0.0)
             #println("Negative energy at time $t")
             #E_ind = findall(x-> x .< 0.0, E)
             #@show E[E_ind]
             #return E
-        elseif any(P .< 0.0)
-            println("Negative pressure at time $t")
-            @show b = findall(x-> x .< 0.0, P)
-            return P
-            #break
         end
+        depth_cham_air = 1:1:length(idx_chamber:nx)
+        P[idx_chamber+1:end] .= (γ .- 1.0) .* (E[idx_chamber+1:end] .- 0.5 .* ρ[idx_chamber+1:end] .* av_x(Vx)[idx_chamber+1:end].^2)
+        P[idx_chamber] = P[idx_chamber+1]
+        #L_t = E[idx_chamber-1:idx_chamber+1]
+        #R_t = 0.5 .* ρ[idx_chamber-1:idx_chamber+1] .* av_x(Vx)[idx_chamber-1:idx_chamber+1].^2
 
-        P[Int((34/100)*nx):end] .= (γ .- 1.0) .* (E[Int((34/100)*nx):end] .- 0.5 .* ρ[Int((34/100)*nx):end] .* av_x(Vx)[Int((34/100)*nx):end].^2)
-        L_t = E[34:36]
-        R_t = 0.5 .* ρ[34:36] .* av_x(Vx)[34:36].^2
+        dP = dρ[1:idx_solid-1] .* c.^2.0
+        P[2:idx_solid] .= P[2:idx_solid] .+ dP 
+        #P[1:idx_solid] .= .-K .* log.(ρ[1:idx_solid] ./ ρ0) .+ P0_s
 
-        #P[1:Int((33/100)*nx)] .= ρ[1:Int((33/100)*nx)] .* c.^2.0
-        dP = dρ[1:Int((33/100)*nx)] .* c.^2.0
-        P[1:Int((33/100)*nx)] .= dP .+ P[1:Int((33/100)*nx)]
-        #P[1:Int((33/100)*nx)] .= ((1.0 ./ β[1:Int((33/100)*nx)]) .* log.(ρ[1:Int((33/100)*nx)] ./ 2800.0) .+ P[1:Int((33/100)*nx)])
-
-        P[1] = 0#P[2]
-        P[end] = 0#P[end-1]
+        #P[1] = 0#P[2]
+        #P[end] = 0#P[end-1]
 
         e = P ./ (γ - 1.0) ./ ρ
 
@@ -227,12 +247,13 @@ function coupled_wave1D_up()
         #    divisor = 1
         #end
         if i % divisor == 0
+            println("#------- Iteration $i-------#")
             @show c_var_s
-            @show c_var_a
-            mach = maximum(abs.(Vx[34:end])) / 340.0
+            @show c_var_c
+            mach = maximum(abs.(Vx[idx_chamber-1:end])) / 340.0
             @show mach
-            @show i, L_t
-            @show i, R_t
+            @show av_x(Vx)[idx_chamber-1:idx_chamber+1].^2
+            @show ρ[idx_chamber-1:idx_chamber+1]
             fig2 = Figure(size=(1000, 800))
             ax1 = Axis(fig2[2,1], title="Pressure, time = $(round(t, digits=8))")#, limits=(nothing, nothing, -0.25, 0.25))
             ax2 = Axis(fig2[1,2], title="Velocity")#, limits=(nothing, nothing, -0.25, 0.25))
